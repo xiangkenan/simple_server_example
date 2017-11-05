5943b798296437cc8cb615ead361b011f5a4f027
root <root@iZ2ze8hspx8qsmz8k1qtuvZ.(none)>
2017-11-05 17:31:24 +0800

    ad

---
+++ ctl.sh
@@ -0,0 +1,20 @@
+#! /bin/bash
+
+#make clean;
+#make;
+##./run -b 192.168.30.236:9092 -t userevents -p 1
+#./run
+
+proc="./crm_noah"
+
+if [[ $1 == "stop" ]]; then
+    ps -aux | grep "$proc" | grep -v grep | awk '{print $2}' | xargs kill -9
+elif [[ $1 == "restart" ]]; then
+    ps -aux | grep "$proc" | grep -v grep | awk '{print $2}' | xargs kill -9
+    sleep 2
+    nohup $proc 2>&1 > ./log/run.log &
+elif [[ $1 == "start" ]]; then
+    nohup $proc 2>&1 ./log/run.log &
+else
+    echo "please input start|restart|stop?"
+fi
---
+++ src/city.h
@@ -0,0 +1,13 @@
+#ifndef CITY_H_
+#define CITY_H_
+
+#include <iostream>
+#include <map>
+
+class City {
+    public:
+        City();
+        std::map<std::string, std::string> city_map;
+};
+
+#endif
