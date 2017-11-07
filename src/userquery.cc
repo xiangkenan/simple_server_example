6c584f5057550c0656bd07f2fb3cb9218de41efb
root <root@cloud-fengkong-dev.(none)>
2017-11-07 12:02:33 +0800

    ad

--- src/userquery.cc
+++ src/userquery.cc
@@ -105,23 +105,23 @@ bool UserQuery::SendMessage(KafkaData* kafka_data, Redis* redis_user_trigger_con
                 string url = "192.168.2.123/now";
                 string args = "to=" + kafka_data->tel + "&templateId=crm_notify&context="+tel_push_msg[j].content;
                 string token = "x-ofo-token:eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxODYzNjY0Nzk2MiIsIm5hbWUiOiLmt7HlnLMifQ.EjXJEjEWGKcsI896Mx6BUCbtnlq_gcnQ2NjpQaZSLkE";
-                //*************
-                if ((ret = murl_get_url(url.c_str(), buf, 10240, 0, NULL, token.c_str(), args.c_str())) != MURLE_OK) {
-                    kafka_data->log_str += "{send message error!!}";
-                }
-                //*************
+                ////*************
+                //if ((ret = murl_get_url(url.c_str(), buf, 10240, 0, NULL, token.c_str(), args.c_str())) != MURLE_OK) {
+                //    kafka_data->log_str += "{send message error!!}";
+                //}
+                ////*************
                 kafka_data->log_str += "●●{send message to " + kafka_data->tel + ":" + 
                     tel_push_msg[j].content + "}";
             }
             if (tel_push_msg[j].type == "push") {
-                int id = (atoi(kafka_data->uid.c_str()))%1+1;
-                Json::Value push_json;
-                Json::FastWriter writer;
-                push_json["cid"] = kafka_data->uid+ kafka_data->tel;
-                push_json["content"] = tel_push_msg[j].content;
-                push_json["jump_url"] = tel_push_msg[j].jump_url;
-                string push_json_str = writer.write(push_json);
-                redis_user_trigger_config->Lpush("push:"+to_string(id), push_json_str);
+                //int id = (atoi(kafka_data->uid.c_str()))%1+1;
+                //Json::Value push_json;
+                //Json::FastWriter writer;
+                //push_json["cid"] = kafka_data->uid+ kafka_data->tel;
+                //push_json["content"] = tel_push_msg[j].content;
+                //push_json["jump_url"] = tel_push_msg[j].jump_url;
+                //string push_json_str = writer.write(push_json);
+                //redis_user_trigger_config->Lpush("push:"+to_string(id), push_json_str);
                 
                 kafka_data->log_str += "●●{send push to "+kafka_data->uid+":" + 
                     tel_push_msg[j].content + "jump_url:" + tel_push_msg[j].jump_url+ "}";
